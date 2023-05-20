4785f312f715e788859ce2917b2d3e9938ff6e23
adnwhn <dianadoitrei@gmail.com>
2023-05-20 23:36:06 +0300

    windows

---
+++ MDSProject/Grade.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MDSProject
+{
+    /// <summary>
+    /// Interaction logic for Grade.xaml
+    /// </summary>
+    public partial class Grade : Window
+    {
+        public Grade()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
